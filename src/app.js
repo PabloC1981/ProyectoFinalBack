bfa69130986b7fcc7ffe834b7fdf508aae18d98e
PabloC1981 <pablocourroux@gmail.com>
2022-03-22 07:33:00 -0300

    Crud/User/Cart/Product-PRO

--- src/app.js
+++ src/app.js
@@ -7,10 +7,13 @@ import productRouter from './routes/products.js'
 import cartRouter from './routes/carts.js'
 import cookieParser from 'cookie-parser';
 import { __dirname } from './utils/utils.js';
+import { createLogger } from './utils/utils.js';
+import config from './config/config.js';
+
 const app = express();
 const PORT = process.env.PORT||8080;
 const server = app.listen(PORT,()=>console.log(`Listening on ${PORT}`));
-
+const logger = createLogger(config.env.NODE_ENV)
 
 app.use(express.json());
 app.use(express.static(__dirname+'/public'));
@@ -23,3 +26,35 @@ app.use('/session',sessionRouter)
 app.use('/api/users',usersRouter)
 app.use('/api/product',productRouter)
 app.use('/api/cart',cartRouter)
+
+
+
+// /*renderizo front! */
+app.get('/login',(req,res)=>{
+    res.sendFile(__dirname+'/public/pages/login.html')
+})
+
+app.get('/register',(req,res)=>{
+    res.sendFile(__dirname+'/public/pages/register.html')
+})
+
+app.get('/view/cart',(req,res)=>{
+    res.sendFile(__dirname+'/public/pages/cart.html')
+})
+
+
+
+ /*Configuro Winston como logger*/
+app.use((req,res,next)=>{
+    logger.log('info',`${req.method} at ${req.path}`)
+    next()
+});
+
+app.on('error',(error)=>{
+    logger.warn("warning, check please")
+    console.log("Danger: "+error)
+})
+app.get('/*',(req,res)=>{
+    logger.warn("Endpoint invalido")
+    res.status(404).send({error:'Invalid endpoint'})
+})
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import winston from 'winston';
 
 const filename= fileURLToPath(import.meta.url);
 export const __dirname = dirname(filename);
@@ -13,4 +14,21 @@ export const cookieExtractor = req =>{
         token=req.cookies["JWT_COOKIE"];
     }
     return token;
+};
+export const createLogger=(env) =>{
+    if(env === "PROD"){
+        return winston.createLogger({
+            transports:[
+                new winston.transports.File({ filename:'loggers/combined.log'}),
+                new winston.transports.File({filename:"loggers/errors.log",level:"error"})
+            ]
+        })
+    }else{
+        return winston.createLogger({
+            level:'info',
+            transports:[
+                new winston.transports.Console({level:"info"})
+            ]
+        })
+    }
 }
